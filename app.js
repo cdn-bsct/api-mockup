2f645368756c0b4202f55e77ced73e8729563923
Justin Feldman <justin.feldman@shyftlabs.io>
2023-06-02 15:10:11 -0400

    updated fetch request

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let queryData = null;
-let fetch_url = "https://pricing-engine-staging-api.petc.com/graphql";
+let fetch_url = "https://staging-pricing.petco.com"
 // GQL Query Elements
 let locationId = 6989;
 let skuId = 2471105;
@@ -8,30 +8,32 @@ async function itemQuery(location, sku) {
   await fetch(fetch_url, {
     method: "POST",
     headers: {
+      "x-api-key": "X5hbNvtra15hv69pH7ZK23QCxK5g0Z1m6CjR27VU",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
       query: `query {
-                getPriceByLocation(locationIds: "${location}", skuIds: "${sku}") {
-                  data {
-                    location
-                    item
-                    listPrice
-                    salePrice
-                    basePrice
-                    recommendedPrice
-                    rdPrice
-                    markdownPrice
-                    lastUpdatedDateTime
-                    createdDateTime
-                    boposPrice
+                  getPriceByLocation(locationIds: "${location}", skuIds: "${sku}") {
+                    data {
+                      location
+                      item
+                      listPrice
+                      salePrice
+                      basePrice
+                      recommendedPrice
+                      rdPrice
+                      markdownPrice
+                      lastUpdatedDateTime
+                      createdDateTime
+                      boposPrice
+                    }
                   }
-                }
-              }`,
+                }`,
     }),
   })
     .then((res) => res.json())
-    .then((res) => (queryData = res.data.getPriceByLocation.data[0]));
+    .then((res) => (queryData = res.data.getPriceByLocation.data[0]))
+    .catch((err) => console.log(err));
 }
 
 async function changePrice(data) {
